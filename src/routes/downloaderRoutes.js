18f033b7c10f6d6ddf2f4d952694247e9a5e121a
rznive <rezaofc.jr@gmail.com>
2025-09-17 17:50:56 +0700

    remove typo in line 3 downloaderRoutes

--- src/routes/downloaderRoutes.js
+++ src/routes/downloaderRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const downloaderController = require("../src/controllers/downloaderController");
+const downloaderController = require("../controllers/downloaderController");
 
 // Rute untuk mendownload video TikTok menggunakan metode GET
 router.get("/tiktokdl", downloaderController.downloadTikTokVideo);
